f024ca0ead0ea13a5b40ee068973f3ef1abf7a3f
Biplab Saha <111464784+biplab-saha@users.noreply.github.com>
2024-12-20 20:32:34 +0530

    all are new

---
+++ 002/newCode.java
@@ -0,0 +1,17 @@
+
+public class newCode {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to new iourney ");
+        String i = "Biplab Saha";
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(i);
+        }
+        int a = 3030;
+        int b = 3305;
+        a = b;
+        System.out.println(b);
+        System.out.println(a);
+    }
+
+}
