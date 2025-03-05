2698ac4b2c6c78f54d801177fd1696f5a8cfe216
Biplab Saha <biplab5523x@gmil.com>
2025-03-05 15:29:03 +0530

    justOneFileAdd

---
+++ biplab_kunal/01/starPatten.java
@@ -0,0 +1,13 @@
+public class starPatten {
+    public starPatten(){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+    
+}
